1cecb120a9a235bd2f25e434dc3d1a27a486687d
vnfmsqkek3 <czy1023@gmail.com>
2022-07-22 15:40:26 +0900

    [FEAT] GET / 기능 추가

---
+++ server-backend/router/tweets.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import 'express-async-errors';
+
+const tweets = [{
+    id:'1',
+    text: "test용 텍스트",
+    createdAt: Date.now().toString(),
+    name: 'jaehyeok',
+    username: 'vnfmsqkek3',
+    url: 'https://www.linkedin.com/in/%EC%9E%AC%ED%98%81-%EC%B5%9C-515606241/'
+},
+];
+
+const router = express.Router();
+
+// GET /tweets
+// GET /tweets?username=:username
+router.get('/', (req, res, next) => {
+    const username = req.query.username;
+
+    //username이 있다면 현재 가지고 있는 tweets에서 filter를 해준다.
+    //가지고 있는 배열의 아이템이 트윗을 전달받아서 트윗에 있는 username이 사용자가 원하는 username과 동일한 것만 골라낸다
+    //username이 없는 경우라면 tweets를 할당한다
+    const data = username 
+    ? tweets.filter(t => t.username === username)
+    : tweets;
+    res.status(200).json(data);
+});
+
+// GET /tweets/:id
+// POST /tweets
+// PUT /tweets/:id
+// DELETE /tweets/:id
+
+
+export default router;
