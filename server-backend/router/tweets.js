854ecce8fa80727b92eb50172665052512437db7
vnfmsqkek3 <czy1023@gmail.com>
2022-07-22 16:03:21 +0900

    [FEAT] POST / 기능 추가

--- server-backend/router/tweets.js
+++ server-backend/router/tweets.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 
-const tweets = [{
+let tweets = [{
     id:'1',
     text: "test용 텍스트",
     createdAt: Date.now().toString(),
@@ -16,7 +16,7 @@ const tweets = [{
     name: 'Zero',
     username: 'ttt',
     
-}
+},
 ];
 
 const router = express.Router();
@@ -45,8 +45,21 @@ router.get('/:id', (req, res, next) => {
     else {
         res.status(404).json({message: "Tweet ${id} not found"});
     }
-})
+});
+
 // POST /tweets
+router.post('/', (req, res, next) => {
+    const { text, name, username } = req.body;
+    const tweet = {
+        id: Date.now().toString(), //mysql id를 대체
+        text,
+        createdAt: new Date(),
+        name,
+        username,
+    };
+    tweets = [tweet, ...tweets];
+    res.status(201).json(tweet);
+})
 // PUT /tweets/:id
 // DELETE /tweets/:id
 
