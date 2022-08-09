622fe8cc4e0f5529e5ba4b8be976afb5c6ce0978
vnfmsqkek3 <czy1023@naver.com>
2022-08-09 12:44:16 +0900

    [EDIT] 이름 공백란 유효성검사

--- validation/app.js
+++ validation/app.js
@@ -6,16 +6,21 @@ app.use(express.json());
 
 //새로운 사용자를 만듬
 //express-valildator를 이용해 유효성을 검사하는 코드 작성 
-app.post('/users', 
-body('name').isLength({min:2, max: 10}).withMessage('이름은 2글자이상 10글자 이하 '), 
-(req, res, next) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        res.status(400).json({ message: errors.array() });
-    }
-    console.log(req.body);
-    res.status(201);
-});
+app.post(
+    '/users',
+    body('name')
+        .notEmpty()
+        .withMessage('이름을 입력해 주세요')
+        .isLength({ min: 2, max: 10 })
+        .withMessage('이름은 2글자이상 10글자 이하 '),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).json({ message: errors.array() });
+        }
+        console.log(req.body);
+        res.status(201);
+    });
 
 //사용자의 email을 볼 수 있는 코드
 app.get('/:email', (req, res, next) => {
