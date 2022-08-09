a674b401c90f79203aca0b54bc6721afec21f3ac
vnfmsqkek3 <czy1023@naver.com>
2022-08-09 13:12:24 +0900

    [FEAT] 코드 리펙토링

--- validation/app.js
+++ validation/app.js
@@ -4,6 +4,16 @@ import { body, check, param, validationResult } from 'express-validator';
 const app = express();
 app.use(express.json());
 
+//코드 리펙토링
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        return next();
+    }
+    return res.status(400).json({ message: errors.array()[0].msg });
+};
+
+
 //새로운 사용자를 만듬
 //express-valildator를 이용해 유효성을 검사하는 코드 작성 
 app.post(
@@ -13,12 +23,10 @@ app.post(
         body('age').notEmpty().isInt().withMessage("숫자를 입력해주세요"),
         body('email').isEmail().withMessage("이메일을 입력해주세요"),
         body('job.name').notEmpty(),
+        validate
     ],
     (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ message: errors.array() });
-        }
+
         console.log(req.body);
         res.status(201);
     });
@@ -26,13 +34,9 @@ app.post(
 //사용자의 email을 볼 수 있는 코드 (localhost:8080/czy1023@gmail.com)
 app.get('/:email',
     param('email').isEmail().withMessage("이메일을 입력해주세요"),
-    //check('email').isEmail().withMessage("이메일을 입력해주세요"),
     (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ message: errors.array() });
-        }
-        res.send("email !")
+    validate,
+    res.send("email !")
     });
 
 app.listen(8080);
