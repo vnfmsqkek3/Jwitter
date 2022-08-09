cbe5ae8b47002ac09482ab6c5677aca6638a1f14
vnfmsqkek3 <czy1023@naver.com>
2022-08-09 12:38:50 +0900

    [EDIT] express-validator를 이용한 이름길이 유효성검사

--- validation/app.js
+++ validation/app.js
@@ -1,18 +1,20 @@
 import express from 'express';
+import { body, validationResult } from 'express-validator';
 
 const app = express();
 app.use(express.json());
 
 //새로운 사용자를 만듬
-//이메일의 유효성을 검사하는 코드 작성
-app.post('/users', (req, res, next) => {
+//express-valildator를 이용해 유효성을 검사하는 코드 작성 
+app.post('/users', 
+body('name').isLength({min:2, max: 10}).withMessage('이름은 2글자이상 10글자 이하 '), 
+(req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        res.status(400).json({ message: errors.array() });
+    }
     console.log(req.body);
-    if(req.body.email.indexOf("@" != -1)) {
-        res.status(201).send({ message : 'hello!'})
-    }
-    else{
-    res.sendStatus(401).send({ message : "not email! "});
-    }
+    res.status(201);
 });
 
 //사용자의 email을 볼 수 있는 코드
