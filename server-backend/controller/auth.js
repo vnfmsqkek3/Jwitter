c47e6af8ce8bb79548fb8d6b31c5bdb25cfc40cb
vnfmsqkek3 <czy1023@naver.com>
2022-08-10 10:34:46 +0900

    [FEAT] server-backend JWT 기능 추가

--- server-backend/controller/auth.js
+++ server-backend/controller/auth.js
@@ -3,26 +3,32 @@ import bcrypt from 'bcrypt';
 import {} from 'express-async-errors';
 import * as userRepository from '../data/auth.js';
 
-const jwtSecretKey = ''; //secret key
+const jwtSecretKey = 'F2dN7x8HVzBWaQuEEDnhsvHXRWqAR63z'; //secret key
 const jwtExpiresInDays = '2d'; //만료시간
 const bcryptSaltRounds = 12; //bcrypt 길이
 
-export async function signup(req, res) {
-    const { username, password, name, email, url } = req.body;
-    const found = await userRepository.findByUsername(username);
-    if (found) {
-        return res.status(409).json({ message: `${username} already exists`});
+export async function login(req, res) {
+    const { username, password } = req.body;
+    const user = await userRepository.findByUsername(username);
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid user or password' });
     }
-
-    const hashed = await bcrypt.hash(password, bcryptSaltRounds);
-    const userId = await userRepository.createUser({
-        username,
-        password: hashed,
-        name,
-        email,
-        url,
-    });
-    const token = createJwtToken(userId);
-    res.status(201).json({ token, username});
-
-}
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return res.status(401).json({ message: 'Invalid user or password' });
+    }
+    const token = createJwtToken(user.id);
+    res.status(200).json({ token, username });
+  }
+  
+  function createJwtToken(id) {
+    return jwt.sign({ id }, jwtSecretKey, { expiresIn: jwtExpiresInDays });
+  }
+  
+  export async function me(req, res, next) {
+    const user = await userRepository.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json({ token: req.token, username: user.username })
+};
--- server-backend/data/auth.js
+++ server-backend/data/auth.js
@@ -2,19 +2,23 @@ let users = [
     {
         id: '1',
         username: 'vnfmsqkek3',
-        password: '',
+        password: '$2b$10$8GpdlpSKuG7GAssEJswRtu1k6j6AivENg8cgjMbWCfNjiO9ktGJNa',
         name: 'jaehyeok',
         email: 'czy1023@gmail.com',
         url : 'https://github.com/vnfmsqkek3'
     },
 ];
 
-export async function findByUsername(username){
+export async function findByUsername(username) {
     return users.find((user) => user.username === username);
-}
-
-export async function createUser(user) {
+  }
+  
+  export async function findById(id) {
+    return users.find((user) => user.id === id);
+  }
+  
+  export async function createUser(user) {
     const created = { ...user, id: Date.now().toString() };
     users.push(created);
     return created.id;
-}
+  }
