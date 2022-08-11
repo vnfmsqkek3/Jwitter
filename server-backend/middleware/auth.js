dba3d67b30c1a1292c703d75e96f60cd9dbf84f6
vnfmsqkek3 <czy1023@naver.com>
2022-08-11 11:09:34 +0900

    [FEAT] isAuth 기능 추가

--- server-backend/middleware/auth.js
+++ server-backend/middleware/auth.js
@@ -4,26 +4,26 @@ import * as userRepository from '../data/auth.js';
 const AUTH_ERROR = { message: 'Authentication Error' };
 
 export const isAuth = async (req, res, next) => {
-  const authHeader = req.get('Authorization');
-  if (!(authHeader && authHeader.startsWith('Bearer '))) {
-    return res.status(401).json(AUTH_ERROR);
-  }
-
-  const token = authHeader.split(' ')[1];
-
-  jwt.verify(
-    token,
-    'F2dN7x8HVzBWaQuEEDnhsvHXRWqAR63z',
-    async (error, decoded) => {
-      if (error) {
+    const authHeader = req.get('Authorization');
+    if (!(authHeader && authHeader.startsWith('Bearer '))) {
         return res.status(401).json(AUTH_ERROR);
-      }
-      const user = await userRepository.findById(decoded.id);
-      if (!user) {
-        return res.status(401).json(AUTH_ERROR);
-      }
-      req.userId = user.id; // req.customData
-      next();
     }
-  );
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(
+        token,
+        'F2dN7x8HVzBWaQuEEDnhsvHXRWqAR63z',
+        async (error, decoded) => {
+            if (error) {
+                return res.status(401).json(AUTH_ERROR);
+            }
+            const user = await userRepository.findById(decoded.id);
+            if (!user) {
+                return res.status(401).json(AUTH_ERROR);
+            }
+            req.userId = user.id; // req.customData
+            next();
+        }
+    );
 };
