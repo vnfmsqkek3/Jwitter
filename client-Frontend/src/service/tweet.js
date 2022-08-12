f1664658a6461ddba02963451ba8c2b70e854a1b
vnfmsqkek3 <czy1023@naver.com>
2022-08-12 11:02:59 +0900

    [FEAT] frontend/src/service/tweets.js  headers,tokenStorage 추가

--- client-Frontend/src/service/tweet.js
+++ client-Frontend/src/service/tweet.js
@@ -2,7 +2,7 @@ export default class TweetService {
 
   // tweet API를 사용하는 프론트앤드 구현
 
-  constructor(http){
+  constructor(http, tokenStorage) {
     this.http = http;
   }
 
@@ -10,26 +10,37 @@ export default class TweetService {
     const query = username ? `?username=${username}` : '';
     return this.http.fetch(`/tweets${query}`, {
       method: 'GET',
+      headers: this.getHeaders(),
     });
   }
 
   async postTweet(text) {
     return this.http.fetch(`/tweets`, {
       method: 'POST',
-      body: JSON.stringify({text,username: 'jaehyeok',name: 'Jaehyeok'}),
+      headers: this.getHeaders(),
+      body: JSON.stringify({ text, username: 'jaehyeok', name: 'Jaehyeok' }),
     });
   }
 
   async deleteTweet(tweetId) {
     return this.http.fetch(`/tweets${tweetId}`, {
       method: 'DELETE',
+      headers: this.getHeaders(),
     });
   }
 
   async updateTweet(tweetId, text) {
     return this.http.fetch(`/tweets/${tweetId}`, {
       method: 'PUT',
+      headers: this.getHeaders(),
       body: JSON.stringify({ text }),
     });
+  }
+
+  getHeaders() {
+    const token = this.tokenStorage.getToken();
+    return {
+      Authorization: `Bearer ${token}`,
+    };
   }
 }
