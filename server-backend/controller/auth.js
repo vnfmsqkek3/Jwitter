6221f1a647a1ebd84db6685649da6bc366798d59
vnfmsqkek3 <czy1023@naver.com>
2022-08-17 12:04:18 +0900

    [FEAT] config.js에 변수가 설정이 안되있다면 알려주는 코드 작성

--- server-backend/controller/auth.js
+++ server-backend/controller/auth.js
@@ -6,10 +6,10 @@ import { config } from '../config.js'
 
 
 export async function signup(req, res) {
-    const { username, password, name, email, url} = req.body;
+    const { username, password, name, email, url } = req.body;
     const found = await userRepository.findByUsername(username);
-    if(found){
-        return res.status(409).json({ message: `${username} already exists`});
+    if (found) {
+        return res.status(409).json({ message: `${username} already exists` });
     }
     const hashed = await bcrypt.hash(password, config.bcrypt.saltRounds);
     const userId = await userRepository.createUser({
@@ -20,24 +20,24 @@ export async function signup(req, res) {
         url,
     });
     const token = createJwtToken(userId);
-    res.status(201).json({ token, username});
+    res.status(201).json({ token, username });
 }
 
 export async function login(req, res) {
     const { username, password } = req.body;
     const user = await userRepository.findByUsername(username);
     if (!user) {
-        return res.status(401).json({ messgae: 'ID나 패스워드가 잘못되었습니다.'});
+        return res.status(401).json({ messgae: 'ID나 패스워드가 잘못되었습니다.' });
     }
     const isValidPassword = await bcrypt.compare(password, user.password);
-    if(!isValidPassword) {
-        return res.status(401).json({ message: 'ID나 패스워드가 잘못되었습니다.'});
+    if (!isValidPassword) {
+        return res.status(401).json({ message: 'ID나 패스워드가 잘못되었습니다.' });
     }
     const token = createJwtToken(user.id);
-    res.status(200).json({ token, username});
+    res.status(200).json({ token, username });
 }
 
-function createJwtToken(id){
+function createJwtToken(id) {
     console.log(config.jwt.secretKey);
     return jwt.sign({ id }, config.jwt.secretKey, { expiresIn: config.jwt.expiresInSec });
 }
@@ -47,5 +47,5 @@ export async function me(req, res, next) {
     if (!user) {
         return res.status(404).json({ message: 'User not found' });
     }
-    res.status(200).json({ token: req.token, username: user.username});
+    res.status(200).json({ token: req.token, username: user.username });
 }
