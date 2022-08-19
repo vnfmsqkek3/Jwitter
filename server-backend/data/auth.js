db557f91bde3a00b8b59cc37c4ff3eb0b85b9a39
vnfmsqkek3 <czy1023@naver.com>
2022-08-19 10:09:51 +0900

    [FEAT] server/data/auth.js findByUsername 생성

--- server-backend/data/auth.js
+++ server-backend/data/auth.js
@@ -1,8 +1,8 @@
 import { db } from '../db/database.js';
 
 export async function findByUsername(username) {
-    return null;
-    //return users.find((user) => user.username === username);
+    return db.execute('SELECT * FROM users WHERE username=?', [username])
+    .then((result) => result[0][0]);
 }
 
 export async function findById(id) {
@@ -14,8 +14,5 @@ export async function createUser(user) {
     const { username, password, name, email, url } = user;
     return db.execute('INSERT INTO users (username, password, name, email, url) VALUES (?,?,?,?,?)',
         [username, password, name, email, url]
-    ).then((result) => {
-        console.log(result);
-        return result;
-    });
+    ).then((result) =>  result[0].insertId);
 }
