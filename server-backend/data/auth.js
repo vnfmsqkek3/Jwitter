5346e16144c80865581fdc4c2d366a2342c31957
vnfmsqkek3 <czy1023@naver.com>
2022-08-22 12:49:05 +0900

    [FEAT] server/data/tweet.js getAll, getAllByUsername, GetByID, create db용 수정 완료

--- server-backend/data/auth.js
+++ server-backend/data/auth.js
@@ -1,18 +1,23 @@
 import { db } from '../db/database.js';
 
 export async function findByUsername(username) {
-    return db.execute('SELECT * FROM users WHERE username=?', [username])
+  return db
+    .execute('SELECT * FROM users WHERE username=?', [username]) //
     .then((result) => result[0][0]);
 }
 
 export async function findById(id) {
-    return null;
-    //return users.find((user) => user.id === id);
+  return db
+    .execute('SELECT * FROM users WHERE id=?', [id]) //
+    .then((result) => result[0][0]);
 }
 
 export async function createUser(user) {
-    const { username, password, name, email, url } = user;
-    return db.execute('INSERT INTO users (username, password, name, email, url) VALUES (?,?,?,?,?)',
-        [username, password, name, email, url]
-    ).then((result) =>  result[0].insertId);
+  const { username, password, name, email, url } = user;
+  return db
+    .execute(
+      'INSERT INTO users (username, password, name, email, url) VALUES (?,?,?,?,?)',
+      [username, password, name, email, url]
+    )
+    .then((result) => result[0].insertId);
 }
