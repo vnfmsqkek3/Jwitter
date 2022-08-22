1280570e484db1194f166283ac6b1eb2e3d5465e
vnfmsqkek3 <czy1023@naver.com>
2022-08-22 12:53:07 +0900

    [FEAT] server/data/tweets.js create,update,remove db용 수정 완료

--- server-backend/data/tweet.js
+++ server-backend/data/tweet.js
@@ -5,33 +5,31 @@ const SELECT_JOIN = 'SELECT tw.id, tw.text, tw.createdAt, us.username, us.name, 
 const ORDER_DESC = 'ORDER BY tw.createdAt DESC'
 export async function getAll() {
     return db.execute(`${SELECT_JOIN} ${ORDER_DESC}`)
-    .then((result) => result[0])
+        .then((result) => result[0])
 };
 
 export async function getAllByUsername(username) {
     return db
-    .execute(`${SELECT_JOIN} WHERE username=? ${ORDER_DESC}`, [username])
-    .then((result) => result[0]);
+        .execute(`${SELECT_JOIN} WHERE username=? ${ORDER_DESC}`, [username])
+        .then((result) => result[0]);
 }
 
 export async function getById(id) {
     return db.execute(`${SELECT_JOIN} WHERE tw.id=?`, [id])
-    .then((result) => result[0][0]);
+        .then((result) => result[0][0]);
 }
 
 export async function create(text, userId) {
-    return db.execute('INSERT INTO tweets (text, createdAt, userId VALUES(?,?,?)', 
-    [text, new Date(), userId])
+    return db.execute('INSERT INTO tweets (text, createdAt, userId VALUES(?,?,?)',
+        [text, new Date(), userId]
+    ).then(result => getById(result[0].insertId));
 }
 
 export async function update(id, text) {
-    const tweet = tweets.find((tweet) => tweet.id === id);
-    if (tweet) {
-        tweet.text = text;
-    }
-    return getById(tweet.id);
+    return db.execute('UPDATE tweets SET text=? WHRE id=?', [text, id])
+    .then(() => getById(id));
 }
 
 export async function remove(id) {
-    tweets = tweets.filter((tweet) => tweet.id !== id);
+    return db.execute('DELETE FROM tweets WHERE id=?', [id])
 }
