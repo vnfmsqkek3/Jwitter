73c61cce0fb7369071fb4f478d6c0159503b4ae3
vnfmsqkek3 <czy1023@naver.com>
2022-09-13 11:40:42 +0900

    [FEAT] client에 axios를 이용한 재시도 구현

--- client-frontend/src/network/http-fetch.js
+++ client-frontend/src/network/http-fetch.js
@@ -1,13 +1,29 @@
 import axios from "axios";
+import { axiosRetry } from "axios-retry";
 
+const defaultRetryConfig = {
+  retries: 5,
+  initialDelayMs: 100,
+}
 export default class HttpClient {
-  constructor(baseURL, authErrorEventBus, getCsrfToken) {
+  constructor(baseURL, authErrorEventBus, getCsrfToken, config = defaultRetryConfig) {
     this.authErrorEventBus = authErrorEventBus;
     this.getCsrfToken = getCsrfToken;
     this.clinet = axios.create({
       baseURL: baseURL,
       headers: { "Content-Type": "application/json" },
       withCredentials: true,
+    });
+    axiosRetry(this.clinet, {
+      retries: config.retries,
+      retryDelay: (retry) => {
+        const delay = Math.pow(2, retry) * config.initialDelayMs; //100, 200, 400, 800, 1600 ...
+        const jitter = delay * 0.1 * Math.random();
+        return delay + jitter;
+      },
+      retryCondition: (err) =>
+        axiosRetry.isNetworkOrIdempotentRequestError(err) ||
+        err.response.status === 429,
     });
   }
 
@@ -31,9 +47,9 @@ export default class HttpClient {
         const data = err.response.datal;
         const message =
           data && data.message ? data.message : "Something went wrong! 🤪";
-          throw new Error(message);
+        throw new Error(message);
       }
-      throw new Error('connection error');
+      throw new Error("connection error");
     }
   }
 }
